6d739ae6183bddb6433aa59bb1799c72d63fc71b
Matthew Daly <matthewbdaly@gmail.com>
2017-10-29 17:53:44 +0000

    Added test settings

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,5 +9,23 @@ class TestCase extends BaseTestCase
 	protected function getPackageProviders($app)
 	{
 		return ['Matthewbdaly\LaravelAzureStorage\AzureStorageServiceProvider'];
-	}
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('filesystems.default', 'azure');
+        $app['config']->set('filesystems.disks.azure', [
+            'driver'    => 'azure',
+            'name'      => 'MY_AZURE_STORAGE_NAME',
+            'key'       => 'MY_AZURE_STORAGE_KEY',
+            'container' => 'MY_AZURE_STORAGE_CONTAINER',
+        ]);
+    }
 }
